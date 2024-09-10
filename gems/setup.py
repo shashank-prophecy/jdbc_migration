4d65fe6f0a53fb32e593a01857690eaab1bd2f1f
shashank@prophecy.io <shashank@prophecy.io>
2024-09-10 08:59:15 +0000

    [Prophecy] Release version 0.8

--- gems/setup.py
+++ gems/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 packages_to_include = find_packages(exclude = ['test.*', 'test', 'test_manual'])
 setup(
     name = 'shashankprophecyioteam_jdbcproject1',
-    version = '10.7',
+    version = '10.8',
     packages = packages_to_include,
     description = '',
     install_requires = [],
